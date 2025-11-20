c00c381bfd6726b0572064176ec2c5c6e87483bc
Baltazar Christian <121530115+T4z4r@users.noreply.github.com>
2025-11-20 15:48:14 +0300

    feat: added tables

---
+++ database/migrations/2025_11_20_124341_create_properties_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('properties', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('landlord_id')->constrained('users')->onDelete('cascade');
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->string('address');
+            $table->string('neighborhood');
+            $table->decimal('geo_lat', 10, 7)->nullable();
+            $table->decimal('geo_lng', 10, 7)->nullable();
+            $table->json('amenities')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('properties');
+    }
+};
