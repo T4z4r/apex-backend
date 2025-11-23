1da59c7b86f6a5246e8d7094a6671e831364605b
Baltazar Christian <121530115+T4z4r@users.noreply.github.com>
2025-11-23 13:31:57 +0300

    fix: minor update

--- routes/web.php
+++ routes/web.php
@@ -39,9 +39,6 @@ Route::get('/lang/{locale}', [LanguageController::class, 'switch'])->name('lang.
 Route::middleware('auth')->group(function () {
 
     Route::get('/dashboard', [DashboardController::class,'index'])->name('dashboard');
-    Route::get('/selects', function () {
-        return view('selects');
-    })->name('selects');
 
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
