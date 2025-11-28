0f34903ec743b4b160e2a03f458cf1201a1dbddd
Baltazar Christian <121530115+T4z4r@users.noreply.github.com>
2025-11-28 15:49:44 +0300

    fix: minor

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -22,7 +22,7 @@
           <div class="col-sm-5 text-center text-sm-left">
             <div class="card-body pb-0 px-0 px-md-4">
               <img
-                src="/assets/img/illustrations/man-with-laptop-light.png"
+                src="{{ asset('assets/img/illustrations/man-with-laptop-light.png') }}"
                 height="140"
                 alt="Property Management"
                 data-app-dark-img="{{ asset('assets/img/illustrations/man-with-laptop-light.png') }}"
